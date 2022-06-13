d536f9c59c817be08a4a1806a9d0340d2c0a03b6
Parteek Saini <sainiparteek18@gmail.com>
2022-06-12 22:30:39 -0700

    Salary App

---
+++ SalaryApp/SalaryApp/Employee.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryApp
+{
+    class Employee
+    {
+        public string EmployeeFN
+        {
+            get; set;
+        }
+        public string EmployeeLN
+        {
+            get; set;
+        }
+        public string EmployeeID
+        {
+            get; set;
+        }
+        public string EmployeeType
+        {
+            get; set;
+        }
+        public double SalaryAmount
+        {
+            get; set;
+        }
+        public double TotalHours
+        {
+            get; set;
+        }
+        public double HourRate
+        {
+            get; set;
+        }
+        public double HourlySalaryCalclator
+        {
+            get
+                
+            {
+                double totalSalary=0;
+                if (TotalHours <= 40)
+                {
+                    totalSalary = HourRate * TotalHours;
+                }
+                else
+                {
+                    totalSalary = HourRate * 40 + (HourRate * 1.5) *(TotalHours - 40);
+                }
+                return totalSalary;
+            }
+        }
+        public double Taxes
+        {
+            get
+            {
+                double taxes=0;
+                if (EmployeeType == "salaried")
+                {
+                    taxes = SalaryAmount * 18 / 100;
+                }
+                if (EmployeeType == "hourly")
+                {
+                    taxes = HourlySalaryCalclator * 18 / 100;
+                }
+                return taxes;
+            }
+        }
+        public double SocialSecurityTax
+        {
+            get
+            {
+                double socialSecurityTax = 0;
+                if (EmployeeType == "salaried")
+                {
+                    socialSecurityTax= SalaryAmount * 6 / 100;
+                }
+                if(EmployeeType == "hourly")
+                {
+                    socialSecurityTax = HourlySalaryCalclator * 6 / 100;
+                }
+                return socialSecurityTax;
+            }
+            
+        }
+        public double NetSalary
+        {
+            get
+            {
+                double netSalary = 0;
+                if (EmployeeType == "salaried")
+                {
+                    netSalary = SalaryAmount - Taxes - SocialSecurityTax;
+                }
+                if (EmployeeType == "hourly")
+                {
+                    netSalary = HourlySalaryCalclator - Taxes - SocialSecurityTax;
+                }
+                return netSalary;
+            }
+        }
+        public Employee(string firstName,string lastName, string iD,string type,double salaryAmount)
+        {
+            EmployeeFN = firstName;
+            EmployeeLN = lastName;
+            EmployeeID = iD;
+            EmployeeType = type;
+            SalaryAmount = salaryAmount;
+        }
+        public Employee(string firstName, string lastName, string iD, string type, double totalHour
+                        , double hourRate)
+        {
+            EmployeeFN = firstName;
+            EmployeeLN = lastName;
+            EmployeeID = iD;
+            EmployeeType = type;
+            TotalHours = totalHour;
+            HourRate = hourRate;
+        }
+        public override string ToString()
+        {
+            string outputStr;
+            if (EmployeeType == "salaried")
+            {
+                outputStr = string.Format("\n{0,35}: {1}\n", "First Name", EmployeeFN);
+                outputStr += string.Format("{0,35}: {1}\n", "Last Name", EmployeeLN);
+                outputStr += string.Format("{0,35}: {1}\n", "EmployeeID", EmployeeID);
+                outputStr += string.Format("{0,35}: {1}\n", "Employee Type", EmployeeType);
+                outputStr += string.Format("{0,35}: {1:C}\n", "Gross Salary Amount", SalaryAmount);
+                outputStr += string.Format("{0,35}: {1:C}\n", "Taxes", Taxes);
+                outputStr += string.Format("{0,35}: {1:C}\n", "Social Security Tax", SocialSecurityTax);
+                outputStr += string.Format("{0,35}: {1:C}\n", "Net Salary Amount after deduction", NetSalary);
+            }
+            else
+            {
+                outputStr = string.Format("{0,35}: {1}\n", "First Name", EmployeeFN);
+                outputStr += string.Format("{0,35}: {1}\n", "Last Name", EmployeeLN);
+                outputStr += string.Format("{0,35}: {1}\n", "EmployeeID", EmployeeID);
+                outputStr += string.Format("{0,35}: {1}\n", "Employee Type", EmployeeType);
+                outputStr += string.Format("{0,35}: {1:C}\n", "Gross Salary Amount", HourlySalaryCalclator);
+                outputStr += string.Format("{0,35}: {1:C}\n", "Taxes", Taxes);
+                outputStr += string.Format("{0,35}: {1:C}\n", "Social Security Tax", SocialSecurityTax);
+                outputStr += string.Format("{0,35}: {1:C}\n", "Net Salary Amount after deduction", NetSalary);
+
+
+            }
+            return outputStr;
+        }
+    }
+}
---
+++ SalaryApp/SalaryApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using static System.Console;
+
+namespace SalaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char userinput = 'y';
+            while (userinput == 'y')
+            {
+            GetEmployeeDetails(out string FirstName, out string LastName,
+                                          out string ID, out string Type);
+            if (Type == "salaried") 
+            {
+                GetSalariedEmployeeDetails(out double salaryamout);
+                Employee Salariedobject = new Employee(FirstName,LastName,ID,Type,salaryamout);
+                WriteLine(Salariedobject.ToString());
+            }
+            else
+            {
+                GethourlyEmployeeDetails(out double hours, out double hourRate);
+                Employee Salariedobject = new Employee(FirstName, LastName, ID, Type,hours,hourRate);
+                WriteLine(Salariedobject.ToString());
+            }
+                Write("Do you want to continue with another employee(Press y for yes or n for no): ");
+                while(!char.TryParse(ReadLine(),out userinput)||(userinput!='y'&&userinput!='n'))
+                {
+                    Write("Please re-enter your response: ");
+                }
+            }
+            if (userinput == 'n')
+            {
+                Clear();
+                WriteLine("Thank you for using the application...");
+            }
+            ReadKey();
+
+        }
+        static void GetEmployeeDetails(out string FirstName, out string LastName,
+                                          out string ID,out string Type )
+        {
+            Write("Enter Employee First name: ");
+            FirstName = ReadLine();
+
+            Write("Enter Employee Last Name: ");
+            LastName = ReadLine();
+
+            Write("Enter Employee ID: ");
+            ID = ReadLine();
+
+            Write("Enter Employee Type(Enter hourly or salaried for respective fields): ");
+            Type = ReadLine();
+            while (Type != "hourly" && Type!= "salaried")
+            {
+                Write("Please re-enter the Employee Type(Note-Exectly enter salaried or hourly): ");
+                Type = ReadLine();
+            }
+        }
+        static void GetSalariedEmployeeDetails(out double salaryamout)
+        {
+            Write("Enter Salary amount: ");
+            while(!double.TryParse(ReadLine(),out salaryamout) || salaryamout <= 0)
+            {
+                Write("Please re-enter the salary amount: ");
+            }
+        }
+        static void GethourlyEmployeeDetails(out double hours,out double hourRate)
+        {
+            Write("Enter Total hours: ");
+            while (!double.TryParse(ReadLine(), out hours) || hours <= 0)
+            {
+                Write("Please re-enter the total hours: ");
+            }
+            Write("Enter Hour Rate: ");
+            while (!double.TryParse(ReadLine(), out hourRate) || hourRate <= 0)
+            {
+                Write("Please re-enter the hour rate: ");
+            }
+        }
+    }
+}
